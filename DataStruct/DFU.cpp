4edf28d147ee95ffcd6d3c0b336386967f7ade81
Daniel Skýpala <daniel@honza.info>
2022-05-11 17:18:45 +0200

    Added DFU

---
+++ DataStruct/DFU.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct dfu_item {
+    dfu_item * father=NULL;
+    long long rank=0;
+    long long value;
+};
+
+dfu_item * dfu_find(dfu_item * item) {
+    if (item->father == NULL) {
+        return item;
+    } else {
+        item->father = dfu_find(item->father);
+        return item->father;
+    }
+}
+
+bool dfu_same(dfu_item * a, dfu_item * b) {
+    return dfu_find(a) == dfu_find(b);
+}
+
+void dfu_union(dfu_item * a, dfu_item * b) {
+    if (dfu_same(a, b)) {
+        return;
+    } else if (a->rank > b->rank) {
+        b->father = a;
+    } else if (a->rank < b->rank) {
+        a->father = b;
+    } else {
+        a->rank++;
+        b->father = a;
+    }
+}
+
+
+// --------------- TESTS -----------------
+int main() {
+    int n = 10;
+    vector<dfu_item> v(n);
+    for (int i=0; i<n; i++) {
+        v[i].value = i;
+    }
+    for (int i=0; i<n-1; i++) {
+        if (dfu_same(&v[0], &v[i+1])) {
+            cout << "bug!" << endl;
+        }
+        dfu_union(&v[i], &v[i+1]);
+        if (!dfu_same(&v[0], &v[i+1])) {
+            cout << "bug!" << endl;
+        }
+    }
+    
+}
