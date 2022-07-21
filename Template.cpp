8d16d06e727979b09e54d053294ddca069bfccda
5K1PY <daniel@honza.info>
2022-07-21 18:44:15 +0200

    Template update

---
+++ Template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define ALL(a) a.begin(), a.end()
+#define RALL(cont) a.end(), a.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define MOD 1000000007
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    for (ll i=0; i<n; i++) {
+        solve();
+    }
+}
